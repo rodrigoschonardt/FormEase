dbb4344aefe3060fdb9c8a864631b8b486de768f
rodrigoschonardt <roaschon@gmail.com>
2023-01-29 15:17:08 -0300

    Primeiro Commit

---
+++ src/test/java/com/formease/api/FormEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formease.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormEaseApplicationTests {
+
+	@Test
+	void contextLoads()
+	{
+	}
+}
